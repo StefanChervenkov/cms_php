32c6d82e71ad8d0a1dc81394a3278bb5c933e396
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2024-08-21 10:09:39 +0300

    Adding a new page for adding new articles

---
+++ CMS System/new-article.php
@@ -0,0 +1,52 @@
+<?php
+
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<h1>My Blog</h1>
+<hr>
+
+<h2>New Article</h2>
+
+<form  method="post">
+
+    <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Enter title here">
+    </div>
+    <div class="mb-3">
+        <label for="content" class="form-label">Content</label>
+        <textarea class="form-control" id="content" name="content" rows="12"></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label for="published_at" class="form-label">Publication date and time</label>
+        <input type="datetime-local" class="form-control" name="published_at" id="published_at">
+    </div>
+
+    <button type="submit" class="btn btn-primary btn-lg">Add</button>
+
+</form>
+
+
+
+
+<body>
+
+</body>
+
+</html>
